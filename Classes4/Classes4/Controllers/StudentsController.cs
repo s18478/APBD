5b7fa066622aec74b5582da457fb4c81541b3de4
s18478 <s18478@pjwstk.edu.pl>
2020-03-29 16:31:37 +0200

    Get students from database

--- Classes4/Classes4/Controllers/StudentsController.cs
+++ Classes4/Classes4/Controllers/StudentsController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Classes4.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Classes4.Controllers
@@ -6,10 +9,35 @@ namespace Classes4.Controllers
     [Route("api/students")]
     public class StudentsController : Controller
     {
+        private const string connecionString = "Data Source=db-mssql.pjwstk.edu.pl;" +
+                                               "Initial Catalog=s18478;" +
+                                               "Integrated Security=True";
+        
         [HttpGet]
         public IActionResult GetStudents()
         {
-            return Ok();
+            var list = new List<Student>();
+            
+            using (var connection = new SqlConnection(connecionString))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Student";
+                
+                connection.Open();
+                var sdr = command.ExecuteReader();    // SqlDataReader
+
+                while (sdr.Read())
+                {
+                    var st = new Student();
+                    st.IndexNumber = sdr["IndexNumber"].ToString();
+                    st.FirstName = sdr["FirstName"].ToString();
+                    st.LastName = sdr["LastName"].ToString();
+                    
+                    list.Add(st);
+                }
+            }
+            return Ok(list);
         }
     }
 }
