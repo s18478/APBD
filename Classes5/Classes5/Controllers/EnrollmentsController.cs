7d4cbfc1ba9193946ecdf76289eba717fd549e51
s18478 <s18478@pjwstk.edu.pl>
2020-04-06 23:52:58 +0200

    Add student enrollment at studies

--- Classes5/Classes5/Controllers/EnrollmentsController.cs
+++ Classes5/Classes5/Controllers/EnrollmentsController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Data.SqlClient;
+using Classes5.Models;
+using Classes5.Models.DTOs.Requests;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Classes5.Controllers
@@ -6,9 +10,115 @@ namespace Classes5.Controllers
     [Route("api/enrollments")]
     public class EnrollmentsController : ControllerBase
     {
-        public IActionResult Index()
+        private const string connecionString = "Data Source=db-mssql.pjwstk.edu.pl;" +
+                                               "Initial Catalog=s18478;" +
+                                               "Integrated Security=True";
+        
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
         {
-            return Ok();
+            // For response
+            Enrollment enrollment = new Enrollment();
+            
+            using (var conn = new SqlConnection(connecionString))
+            using (var comm = new SqlCommand())
+            {
+                conn.Open();
+                comm.Connection = conn;
+
+                var tran = conn.BeginTransaction();
+                comm.Transaction = tran;
+
+                try
+                {
+                    SqlDataReader sdr;
+
+                    // Does the studies exist ?
+                    comm.CommandText = "SELECT IdStudy FROM Studies WHERE Name = @name";
+                    comm.Parameters.AddWithValue("name", request.Studies);
+                    sdr = comm.ExecuteReader();
+                    
+                    if (!sdr.Read())
+                    {
+                        sdr.Close();
+                        tran.Rollback();
+                        return BadRequest("Studies does not exist !");
+                    }
+                    
+                    enrollment.IdStudy = (int) sdr["IdStudy"];
+                    sdr.Close();
+                    
+                    // Does the enrollment exist ? If it does not exist, add new 
+                    comm.CommandText = "SELECT IdEnrollment, StartDate FROM Enrollment WHERE IdStudy = @idStudy AND Semester = 1";
+                    comm.Parameters.AddWithValue("idStudy", enrollment.IdStudy);
+                    sdr = comm.ExecuteReader();
+
+                    // Does not exist (add new)
+                    if (!sdr.Read())
+                    {
+                        sdr.Close();
+                        
+                        // New enrollment index
+                        comm.CommandText = "SELECT MAX(IdEnrollment) FROM Enrollment";
+                        sdr = comm.ExecuteReader();
+
+                        int id = 1;
+                        
+                        if (sdr.Read() && !DBNull.Value.Equals(sdr[0]))
+                        {
+                            id += (int) sdr[0];
+                        }
+                        
+                        sdr.Close();
+                        
+                        comm.CommandText = "INSERT INTO Enrollment VALUES (@id, 1, @idStudy, @startDate)";
+                        comm.Parameters.AddWithValue("id", id);
+                        comm.Parameters.AddWithValue("startDate", DateTime.Now.Date);
+                        comm.ExecuteNonQuery();
+
+                        enrollment.IdEnrollment = id;
+                        enrollment.StartDate = DateTime.Now;
+                    }
+                    else
+                    {
+                        enrollment.IdEnrollment = (int) sdr["IdEnrollment"];
+                        enrollment.StartDate = (DateTime) sdr["StartDate"];
+                    }
+
+                    enrollment.Semester = 1;
+                    sdr.Close();
+            
+                    // Check the index number (unique)
+                    comm.CommandText = "SELECT IndexNumber FROM Student WHERE IndexNumber = @index";
+                    comm.Parameters.AddWithValue("index", request.IndexNumber);
+                    sdr = comm.ExecuteReader();
+
+                    if (sdr.Read())
+                    {
+                        sdr.Close();
+                        tran.Rollback();
+                        return BadRequest("Index exists in database!");
+                    }
+                    
+                    comm.CommandText = "INSERT INTO Student VALUES (@index, @fname, @lname, @birthdate, @idEnroll)";
+                    comm.Parameters.AddWithValue("fname", request.FirstName);
+                    comm.Parameters.AddWithValue("lname", request.LastName);
+                    comm.Parameters.AddWithValue("birthdate", DateTime.Parse(request.BirthDate));
+                    comm.Parameters.AddWithValue("idEnroll", enrollment.IdEnrollment);
+                    sdr.Close();
+                    comm.ExecuteNonQuery();
+
+                    tran.Commit();
+                }
+                catch (SqlException exc)
+                {
+                    tran.Rollback();
+                    return Problem(exc.Message);
+                }
+            }
+            
+            // If everything is okay return code 201 and enrollment 
+            return Created(enrollment.ToString(), enrollment);
         }
     }
 }
--- Classes5/Classes5/Startup.cs
+++ Classes5/Classes5/Startup.cs
@@ -31,12 +31,14 @@ namespace Classes5
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
