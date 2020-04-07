4bd4f16390138c81fbb37b3dc71c21d188b8066d
s18478 <s18478@pjwstk.edu.pl>
2020-04-07 02:23:13 +0200

    Add service for communication with the database

--- Classes5/Classes5/Controllers/EnrollmentsController.cs
+++ Classes5/Classes5/Controllers/EnrollmentsController.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using Classes5.Models;
 using Classes5.Models.DTOs.Requests;
+using Classes5.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Classes5.Controllers
@@ -11,166 +12,40 @@ namespace Classes5.Controllers
     [Route("api/enrollments")]
     public class EnrollmentsController : ControllerBase
     {
-        private const string connecionString = "Data Source=db-mssql.pjwstk.edu.pl;" +
-                                               "Initial Catalog=s18478;" +
-                                               "Integrated Security=True";
+
+        private IStudentDbService _service;
+
+        public EnrollmentsController(IStudentDbService service)
+        {
+            _service = service;
+        }
         
         [HttpPost]
         public IActionResult EnrollStudent(EnrollStudentRequest request)
         {
-            // For response
-            Enrollment enrollment = new Enrollment();
-            
-            using (var conn = new SqlConnection(connecionString))
-            using (var comm = new SqlCommand())
+            try
             {
-                conn.Open();
-                comm.Connection = conn;
-
-                var tran = conn.BeginTransaction();
-                comm.Transaction = tran;
-
-                try
-                {
-                    SqlDataReader sdr;
-
-                    // Does the studies exist ?
-                    comm.CommandText = "SELECT IdStudy FROM Studies WHERE Name = @name";
-                    comm.Parameters.AddWithValue("name", request.Studies);
-                    sdr = comm.ExecuteReader();
-                    
-                    if (!sdr.Read())
-                    {
-                        sdr.Close();
-                        tran.Rollback();
-                        return BadRequest("Studies does not exist !");
-                    }
-                    
-                    enrollment.IdStudy = (int) sdr["IdStudy"];
-                    sdr.Close();
-                    
-                    // Does the enrollment exist ? If it does not exist, add new 
-                    comm.CommandText = "SELECT IdEnrollment, StartDate FROM Enrollment WHERE IdStudy = @idStudy AND Semester = 1";
-                    comm.Parameters.AddWithValue("idStudy", enrollment.IdStudy);
-                    sdr = comm.ExecuteReader();
-
-                    // Does not exist (add new)
-                    if (!sdr.Read())
-                    {
-                        sdr.Close();
-                        
-                        // New enrollment index
-                        comm.CommandText = "SELECT MAX(IdEnrollment) FROM Enrollment";
-                        sdr = comm.ExecuteReader();
-
-                        int id = 1;
-                        
-                        if (sdr.Read() && !DBNull.Value.Equals(sdr[0]))
-                        {
-                            id += (int) sdr[0];
-                        }
-                        
-                        sdr.Close();
-                        
-                        comm.CommandText = "INSERT INTO Enrollment VALUES (@id, 1, @idStudy, @startDate)";
-                        comm.Parameters.AddWithValue("id", id);
-                        comm.Parameters.AddWithValue("startDate", DateTime.Now.Date);
-                        comm.ExecuteNonQuery();
-
-                        enrollment.IdEnrollment = id;
-                        enrollment.StartDate = DateTime.Now;
-                    }
-                    else
-                    {
-                        enrollment.IdEnrollment = (int) sdr["IdEnrollment"];
-                        enrollment.StartDate = (DateTime) sdr["StartDate"];
-                    }
-
-                    enrollment.Semester = 1;
-                    sdr.Close();
-            
-                    // Check the index number (unique)
-                    comm.CommandText = "SELECT IndexNumber FROM Student WHERE IndexNumber = @index";
-                    comm.Parameters.AddWithValue("index", request.IndexNumber);
-                    sdr = comm.ExecuteReader();
-
-                    if (sdr.Read())
-                    {
-                        sdr.Close();
-                        tran.Rollback();
-                        return BadRequest("Index exists in database!");
-                    }
-                    
-                    comm.CommandText = "INSERT INTO Student VALUES (@index, @fname, @lname, @birthdate, @idEnroll)";
-                    comm.Parameters.AddWithValue("fname", request.FirstName);
-                    comm.Parameters.AddWithValue("lname", request.LastName);
-                    comm.Parameters.AddWithValue("birthdate", DateTime.Parse(request.BirthDate));
-                    comm.Parameters.AddWithValue("idEnroll", enrollment.IdEnrollment);
-                    sdr.Close();
-                    comm.ExecuteNonQuery();
-
-                    tran.Commit();
-                }
-                catch (SqlException exc)
-                {
-                    tran.Rollback();
-                    return Problem(exc.Message);
-                }
+                Enrollment enroll = _service.EnrollStudent(request);
+                return Created(enroll.ToString(), enroll);
             }
-            
-            // If everything is okay return code 201 and enrollment 
-            return Created(enrollment.ToString(), enrollment);
+            catch (Exception exc)
+            {
+                return BadRequest(exc.Message);
+            }
         }
 
         [Route("promotions")]
         [HttpPost]
         public IActionResult PromoteStudents(PromoteStudentRequest request)
         {
-            using (var conn = new SqlConnection(connecionString))
-            using (var comm = new SqlCommand())
+            try
             {
-                conn.Open();
-                comm.Connection = conn;
-                
-                // Does the studies and semester exist ?
-                comm.CommandText = "SELECT IdEnrollment FROM Enrollment " +
-                                   "INNER JOIN Studies ON Enrollment.IdStudy = Studies.IdStudy " +
-                                   "WHERE Name = @Studies AND Semester = @Semester;";
-                comm.Parameters.AddWithValue("Studies", request.Studies);
-                comm.Parameters.AddWithValue("Semester", request.Semester);
-                
-                var sdr = comm.ExecuteReader();
-
-                if (!sdr.Read())
-                {
-                    return NotFound("Incorrect studies or semester!");
-                }
-
-                // Run procedure
-                comm.CommandText = "promoteStudents"; 
-                comm.CommandType = CommandType.StoredProcedure;
-                sdr.Close(); 
-                comm.ExecuteNonQuery();
-                    
-                //Get the final enrollment
-                comm.CommandText = "SELECT IdEnrollment, Semester, Enrollment.IdStudy, StartDate FROM Enrollment " +
-                                       "INNER JOIN Studies ON Enrollment.IdStudy = Studies.IdStudy " +
-                                       "WHERE Name = @Studies AND Semester = @newSemester;"; 
-                comm.CommandType = CommandType.Text; 
-                comm.Parameters.AddWithValue("newSemester", request.Semester + 1); 
-                sdr = comm.ExecuteReader();
-                
-                if (sdr.Read()) 
-                { 
-                    Enrollment enrollment = new Enrollment(); 
-                    enrollment.IdEnrollment = (int) sdr["IdEnrollment"];
-                    enrollment.Semester = (int) sdr["Semester"];
-                    enrollment.IdStudy = (int) sdr["IdStudy"];
-                    enrollment.StartDate = (DateTime) sdr["StartDate"];
-                    
-                    return Created(enrollment.ToString(), enrollment);   
-                }
-                return Problem();
+                Enrollment enroll = _service.PromoteStudents(request);
+                return Created(enroll.ToString(), enroll);
+            }
+            catch (Exception exc)
+            {
+                return NotFound(exc.Message);
             }
         }
     }
