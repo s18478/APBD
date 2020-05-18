6136850add955e5445bd649b985e088adbb11602
s18478 <s18478@pjwstk.edu.pl>
2020-05-19 00:13:15 +0200

    Exercise 10 initial commit

---
+++ Classes10/Classes10/Models/MyDbContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Classes10.Models
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Enrollment> Enrollment { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+        public virtual DbSet<Studies> Studies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=db-mssql.pjwstk.edu.pl;Initial Catalog=s18478;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Enrollment>(entity =>
+            {
+                entity.HasKey(e => e.IdEnrollment)
+                    .HasName("Enrollment_pk");
+
+                entity.Property(e => e.IdEnrollment).ValueGeneratedNever();
+
+                entity.Property(e => e.StartDate).HasColumnType("date");
+
+                entity.HasOne(d => d.IdStudyNavigation)
+                    .WithMany(p => p.Enrollment)
+                    .HasForeignKey(d => d.IdStudy)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Enrollment_Studies");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.IndexNumber)
+                    .HasName("Student_pk");
+
+                entity.Property(e => e.IndexNumber).HasMaxLength(100);
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Password).HasMaxLength(68);
+
+                entity.Property(e => e.RefreshToken).HasMaxLength(36);
+
+                entity.Property(e => e.Salt).HasMaxLength(26);
+            });
+
+            modelBuilder.Entity<Studies>(entity =>
+            {
+                entity.HasKey(e => e.IdStudy)
+                    .HasName("Studies_pk");
+
+                entity.Property(e => e.IdStudy).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
