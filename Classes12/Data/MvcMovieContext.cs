ba67b954eb225afb96541f2cb74cf243793b0bb9
s18478 <s18478@pjwstk.edu.pl>
2020-06-01 13:44:34 +0200

    Added Movie model and MvcMovieContext

---
+++ Classes12/Data/MvcMovieContext.cs
@@ -0,0 +1,20 @@
+﻿using Classes12.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classes12.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
