7f802271f10431231b724e15363ce5bccb54e389
s18478 <s18478@pjwstk.edu.pl>
2020-06-01 15:49:13 +0200

    Added column Rating

--- Classes12/Migrations/20200601134345_Rating.Designer.cs
+++ Classes12/Migrations/20200601134345_Rating.Designer.cs
@@ -4,14 +4,16 @@ using Classes12.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Classes12.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20200601134345_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Classes12/Models/Movie.cs
+++ Classes12/Models/Movie.cs
@@ -16,6 +16,7 @@ namespace Classes12.Models
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
 
